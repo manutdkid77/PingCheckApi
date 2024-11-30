eca0c6594c5da82c3e1c4a9653aabc83558deaad
Nathaniel Nunes <manutdkid77@gmail.com>
2024-11-30 12:51:24 +0530

    Add project files.

---
+++ PingCheckApi/Controllers/PingCheckController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace PingCheckApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingCheckController : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                Ping ping = new Ping();
+                for (var i = 0; i < 100; i++)
+                {
+                    try
+                    {
+                        PingReply reply = await ping.SendPingAsync("1.1.1.1");
+                        if (reply.Status == IPStatus.Success)
+                            return Ok();
+                    }
+                    catch { }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError, "Internet is down");
+        }
+    }
+}
